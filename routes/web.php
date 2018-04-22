2602a4c1aaae0904b76c29e5f5c00b10b4913a72
Youssouf Bentaleb <b.youssouf12@gmail.com>
2018-04-22 09:37:25 +0100

    admin routes

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin', [
+    "uses" => "PublicationController@index",
+    "as" => "admin.rr"]);
